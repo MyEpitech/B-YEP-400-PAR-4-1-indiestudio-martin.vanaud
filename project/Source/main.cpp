4dfaa71913ed686f09c95e5641c1c0a5766de4dc
ValentinDurieux <valentin.durieux@epitech.eu>
2022-05-23 16:37:11 +0200

    feat(map): display map in the main function

--- project/Source/main.cpp
+++ project/Source/main.cpp
@@ -5,21 +5,114 @@
 ** main
 */
 
+// #include "raylib.h"
+
+// int main(void)
+// {
+//     InitWindow(800, 450, "raylib [core] example - basic window");
+
+//     while (!WindowShouldClose())
+//     {
+//         BeginDrawing();
+//             ClearBackground(RAYWHITE);
+//             DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+//         EndDrawing();
+//     }
+
+//     CloseWindow();
+
+//     return 0;
+// }
+
+/*******************************************************************************************
+ *
+ *   raylib [models] example - Cubicmap loading and drawing
+ *
+ *   This example has been created using raylib 1.8 (www.raylib.com)
+ *   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+ *
+ *   Copyright (c) 2015 Ramon Santamaria (@raysan5)
+ *
+ ********************************************************************************************/
+
+#include <iostream>
 #include "raylib.h"
 
 int main(void)
 {
-    InitWindow(800, 450, "raylib [core] example - basic window");
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
 
-    while (!WindowShouldClose())
+    InitWindow(screenWidth, screenHeight, "raylib [models] example - cubesmap loading and drawing");
+
+    Image image = LoadImage("assets/maps/17x15.png");      // Load cubicmap image (RAM)
+    Texture2D cubicmap = LoadTextureFromImage(image);       // Convert image to texture to display (VRAM)
+
+    Mesh mesh = GenMeshCubicmap(image, (Vector3){ 1.0f, 1.0f, 1.0f });
+    Model model = LoadModelFromMesh(mesh);
+
+    // NOTE: By default each cube is mapped to one part of texture atlas
+    Texture2D texture = LoadTexture("assets/cubicmap_atlas.png");    // Load map texture
+    model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;             // Set map diffuse texture
+
+    // Define the camera to look into our 3d world
+    Camera3D camera = { 0 };
+    camera.position = (Vector3){ 0.0f, 20.0f, -10.0f };
+    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+    camera.up = (Vector3){ 0.0f, 0.0f, 90.0f };
+    camera.fovy = 45.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+
+    Vector3 mapPosition = { float(cubicmap.width / 2) * -1, 0.0f, float(cubicmap.height / 2) * -1};          // Set model position
+
+    UnloadImage(image);     // Unload cubesmap image from RAM, already uploaded to VRAM
+
+    SetTargetFPS(60);                       // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())            // Detect window close button or ESC key
     {
+        // Update
+        //----------------------------------------------------------------------------------
+        UpdateCamera(&camera);              // Update camera
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
         BeginDrawing();
+
             ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+            BeginMode3D(camera);
+
+                DrawModel(model, mapPosition, 1.0f, WHITE);
+                DrawGrid(80, 1.0f);
+
+            EndMode3D();
+
+            DrawTextureEx(cubicmap, (Vector2){ screenWidth - cubicmap.width*4.0f - 20, 20.0f }, 0.0f, 4.0f, WHITE);
+            DrawRectangleLines(screenWidth - cubicmap.width*4 - 20, 20, cubicmap.width*4, cubicmap.height*4, GREEN);
+
+            DrawText("cubicmap image used to", 658, 90, 10, GRAY);
+            DrawText("generate map 3d model", 658, 104, 10, GRAY);
+
+            DrawFPS(10, 10);
+
         EndDrawing();
+        //----------------------------------------------------------------------------------
     }
 
-    CloseWindow();
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    UnloadTexture(cubicmap);    // Unload cubicmap texture
+    UnloadTexture(texture);     // Unload map texture
+    UnloadModel(model);         // Unload map model
+
+    CloseWindow();              // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
 
     return 0;
 }
